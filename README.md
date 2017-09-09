66cb2117d5c9fd7335c8e6762750c08dca5b3814
Roberto Lucha <roberto.lucha@uswitch.com>
2017-09-09 23:04:48 +0100

    Adds JSON parsing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # haskell101
+
+## TODO
+- Take room type signature from adventure game and bring it into the project
+- Decide on using mongo or SQL to store the room representation, or just YAML files?
+- Check architecture of other MUDs
--- app/Db.hs
+++ app/Db.hs
@@ -8,26 +8,25 @@ import Database.HDBC
 import Database.HDBC.Sqlite3
 import Data.Aeson (FromJSON, ToJSON)
 
+import Room
+
 data GameUser = GameUser Integer String String
   deriving (Show, Generic)
 
 instance ToJSON GameUser
 instance FromJSON GameUser
 
--- getGameUserDetails :: GameUser -> String
--- getGameUserDetails (GameUser username email) = show username ++ show email
-
-sqlToGameUser::[SqlValue] -> GameUser
+sqlToGameUser :: [SqlValue] -> GameUser
 sqlToGameUser row = GameUser (fromSql $ head row) (fromSql $ row!!1) (fromSql $ row!!2)
 
-getAllUsers:: IO [GameUser]
+getAllUsers :: IO [GameUser]
 getAllUsers = do
   -- this path is absolute to the root of the project it seems
   conn <- connectSqlite3 "db.sql"
   q <- quickQuery' conn "select * from users" []
   return (map sqlToGameUser q)
 
-getUser:: Integer -> IO GameUser
+getUser :: Integer -> IO GameUser
 getUser id = do
   conn <- connectSqlite3 "db.sql"
   q <- quickQuery' conn "select * from users where id = ?" [toSql id]
