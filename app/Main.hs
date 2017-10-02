cb8fb241d50132a1ff32c2c04fd81c1580ec8a5b
Roberto Lucha <roberto.lucha@uswitch.com>
2017-10-02 23:01:07 +0100

    A lot of improvements, currently crashes on unhandled command

--- app/Main.hs
+++ app/Main.hs
@@ -1,58 +1,78 @@
 module Main where
 
-import Models.Room.Room (Room(..), Direction(..), Exit(..), getExitAtDirection, charToDirection)
-import Models.Room.Db (getRoom)
+import Models.Room.Room (Room(..), Direction(..), navigateToRoom, getRoom)
+import Control.Monad()
+import Data.Maybe ()
 
 -- create server / client communication
--- Move between rooms from DB
 
 data State = State {
   room :: Room,
-  hp :: Integer
-}
+  hp :: Integer,
+  roomPath :: [Integer]
+} deriving (Eq)
 
-handleCommands :: State -> IO ()
-handleCommands state = do
-  showStatus state
+data Action = String | NoAction
+
+createShell :: IO String
+createShell = do
   putStr "-> "
-  input <- getLine
-  actions <- createActions input
-  newState <- pure (updateState state actions)
-  handleCommands newState
+  getLine
 
-showStatus :: State -> IO ()
-showStatus state = putStrLn $ description (room state)
+handleCommands :: Maybe State -> IO ()
+handleCommands state =
+  case state of
+    Just s' -> do
+      printStatus s' -- make this only if we have changed rooms or something?
+      input <- createShell
+      newState <- updateState s' input
+      handleCommands newState
+    Nothing ->
+      --Try to restore state from DB? Do not crash!
+      print "Fatal Error"
 
-createActions :: String -> IO ()
-createActions input =
-  case input of
-    "n" -> putStrLn "you go north"
-    "s" -> putStrLn "you go south"
-    _   -> putStrLn "what?"
+printStatus :: State -> IO ()
+printStatus state = do
+  _ <- putStrLn ""
+  _ <- putStrLn $ title (room state)
+  _ <- putStrLn $ "\x1b[32m" ++ "Exits: " ++ roomExitsToString (room state) ++ "\x1b[0m" ++ "\n"
+  _ <- putStrLn $ description (room state)
+  pure ()
 
-updateState :: State -> () -> State
-updateState state actions = state
+maybeUpdateRoom :: State -> Maybe Room -> Maybe State
+maybeUpdateRoom st = fmap stateFromRoom
+    where stateFromRoom :: Room -> State
+          stateFromRoom r = st
+            { room = r
+            , roomPath = roomPath st ++ [uid r]
+            }
 
-pingpong :: Room -> IO()
-pingpong room = do
-  print $ getRoomDescription room
-  a <- getChar
-  handleDirections a
-  nextRoom <- getRoomFromDirection room a
-  pingpong nextRoom
+updateState :: State -> String -> IO (Maybe State)
+updateState state actions = do
+  let r = room state
+  -- [x, y] <- sequence $ fmap (navigateToRoom r) [N, S]
+  [x, y] <- traverse (navigateToRoom r) [N, S]
+
+  xr <- pure $ maybeUpdateRoom state x
+  yr <- pure $ maybeUpdateRoom state y
+
+  case actions of
+    "n" -> pure xr
+    "s" -> pure yr
+    _ -> pure Nothing -- This crashes and it shouldn't ... what to do when not recognised command?
 
 getRoomDescription :: Room -> String
-getRoomDescription room = "" ++ title room ++ " " ++ roomExitsToString room ++ " " ++ description room
+getRoomDescription room' = "" ++ title room' ++ " " ++ roomExitsToString room' ++ " " ++ description room'
 
 roomExitsToString :: Room -> String
-roomExitsToString room = head (map show (exits room))
+roomExitsToString room' = head (map show (exits room'))
 
-getRoomFromDirection :: Room -> Char -> IO Room
-getRoomFromDirection room dir =
-  case getExitAtDirection room  $ charToDirection dir of
-    Just (Exit _ roomID) -> getRoom roomID
-    Nothing -> nothingRoom
-    where nothingRoom = getRoom 4
+-- getRoomFromDirection :: Room -> Char -> IO Room
+-- getRoomFromDirection room dir =
+--   case getExitAtDirection room  $ charToDirection dir of
+--     Just (Exit _ roomID) -> getRoom roomID
+--     Nothing -> nothingRoom
+--     where nothingRoom = getRoom 4
 
 handleDirections :: Char -> IO()
 handleDirections a =
@@ -63,15 +83,21 @@ handleDirections a =
     'w' -> putStrLn ": you are headed west"
     _ -> putStrLn ": Sorry, I don't understand you"
 
-main :: IO()
-main = do
+createInitialState :: IO State
+createInitialState = do
   initialRoom <- getRoom 2
-  initialState <- pure State { -- move this to function
+  pure State {
       room = initialRoom
     , hp = 10
+    , roomPath = []
   }
-  handleCommands initialState
 
+main :: IO()
+main = do
+  initialState <- createInitialState
+  handleCommands $ Just initialState
+
+-- Scotty server example
 
 -- {-# LANGUAGE OverloadedStrings #-}
 --
--- app/Models/Room/Room.hs
+++ app/Models/Room/Room.hs
@@ -3,11 +3,14 @@
 module Models.Room.Room where
 
 import GHC.Generics
-import Control.Monad.IO.Class
-import Control.Applicative ((<$>), (<*>))
+import Control.Monad.IO.Class()
+import Control.Applicative ((<$>))
 import Data.Aeson (decode, FromJSON)
 import Data.ByteString.Lazy (ByteString)
 import Data.List (find)
+import Database.HDBC
+import Database.HDBC.Sqlite3
+import Data.Maybe
 
 data Room = Room {
     name :: String
@@ -32,14 +35,103 @@ data Exit = Exit {
 instance FromJSON Exit
 
 instance Show Exit where
-  show (Exit d r) = show d
+  show (Exit d _) = show d
 
 create :: ByteString -> Maybe Room
 create = decode
 
-getExitAtDirection :: Room -> Direction -> Maybe Exit
-getExitAtDirection room dir = find (\ex -> direction ex == dir) (exits room)
+-- getExitAtDirection :: Room -> Direction -> Maybe Exit
+-- getExitAtDirection room dir = find (\ex -> direction ex == dir) (exits room)
 
+navigateToRoom :: Room -> Direction -> IO (Maybe Room) -- Turn this around and make it a IO (Maybe Room)
+navigateToRoom room dir =
+  sequence $ case find (\exit-> direction exit == dir) (exits room) of
+    Just e -> Just (getRoom $ roomID e)
+    Nothing -> Nothing
+
+sqlToRoom :: ([SqlValue], [[SqlValue]]) -> Room
+sqlToRoom (row, exits') = Room (fromSql $ row!!1) (fromSql $ row!!3) (fromSql $ row!!2) mapExits (fromSql roomID')
+  where roomID' = head row
+        mapExits = catMaybes $ sqlToExit <$> exits'
+
+sqlToExit :: [SqlValue] -> Maybe Exit
+sqlToExit row = case row of
+  [] -> Nothing -- is this the proper type for empty SqlValue ?
+  _ -> Just $ Exit (charToDirection (fromSql $ head row)) (fromSql $ row!!1)
+
+-- getRooms :: IO [Room]
+-- getRooms = do
+-- get all the rooms IDs
+-- map those IDs over getRoom
+
+-- getRooms :: IO [Room]
+-- getRooms = do
+--   conn <- connectSqlite3 "db.sql"
+--   q <- quickQuery' conn "select * from rooms" []
+--   return $ map sqlToRoom q
+
+-- change this to fechtRowAl or fetchRowMap ?
+-- move the connection to a shared space?
+getRoom :: Integer -> IO Room
+getRoom id' = do
+  conn <- connectSqlite3 "db.sql"
+  q <- quickQuery' conn "select * from rooms where id = ?" [toSql id']
+  e <- quickQuery' conn "select direction, exit_room_id from exits where room_id = ?" [toSql id']
+  return $ sqlToRoom (head q, e) -- avoid head q, make it headMay
+
+class FromRow a where
+  readRow :: SqlValue -> a
+
+-- instance FromRow Room where
+--   readRow sqlValue = Room
+--     { name = fromSql $ row !! 1
+--     , description = fromSql $ row !! 2
+--     , exits = absurd
+--     , uid = Nothing
+--     }
+--
+--  instance FromRow Exit where
+--    readRow sqlValue = Exit
+--     {
+--
+--     }
+-- getRoomMap :: Integer -> IO Room
+-- getRoomMap id = do
+--   conn <- connectSqlite3 "db.sql"
+--   stmt <- prepare conn "select * from rooms where id = 2"
+--   room <- fetchAllRowsMap' (execute stmt []) [Map String SqlValue]
+--   print room
+
+-- this returns [Map String SqlValue]
+-- extract it with ...
+-- extractPatternStrings ∷ [Map String SqlValue] → [String]
+-- extractPatternStrings []     = []
+-- extractPatternStrings (m:ms) = toString m : extractPatternStrings ms
+--     where
+--         toString ∷  Map String SqlValue → String
+--         toString m = (fromSql . fromJust . (Map.lookup "word"))∷ String
+
+
+-- how to handle errors?
+createRoom :: Room -> IO ()
+createRoom room = do
+  conn <- connectSqlite3 "db.sql"
+  stmt <- prepare conn "insert into rooms (name, description) values (?,?)"
+  _    <- execute stmt [toSql (name room), toSql (description room)]
+  commit conn
+  disconnect conn
+
+-- createRoomsExits :: Room -> IO ()
+-- createRoomsExits room = do
+--   conn <- connectSqlite3 "db.sql"
+--   roomID <- quickQuery' conn "select id from rooms where name = ?" [toSql (name room)]
+--   stmt <- prepare conn "insert into exits (room_id, north, south, west, east) values (?,?,?,?)" [toSql roomID, (maybe?? exit)]
+--   execute stmt [toSql (name room), toSql (description room)]
+--   commit conn
+--   disconnect conn
+
+
+-- replace fromSql call with a typeclass instance?
 charToDirection :: Char -> Direction
 charToDirection d = case d of
   'n' -> N
